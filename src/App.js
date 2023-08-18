6e0396d3d7a6e559750a0401272054cfb8b7b1e3
runtimeerror55 <runtimeerror55@gmail.com>
2023-08-18 22:23:27 +0530

    added router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
-import "./App.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Home_page } from "./pages/home_page/components/home_page";
 
+const router = createBrowserRouter([
+      {
+            path: "/",
+            element: <Home_page></Home_page>,
+      },
+]);
 function App() {
-      return <div className="App">hello</div>;
+      return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ src/pages/home_page/components/home_page.js
@@ -0,0 +1,3 @@
+export const Home_page = () => {
+      return "hello ";
+};
